2f4da8169c2df1b61d9e171aa5e1b7f06671fd79
sharanjeetkaur <sharanjeet.kaur@lt.hs-fulda.de>
2023-04-26 12:52:00 +0200

    fake content is working

--- pages/api/utils.js
+++ pages/api/utils.js
@@ -29,9 +29,64 @@ import {faker} from '@faker-js/faker'
         count++
       }
     }
-    if(req.body.task === 'add_fake_content'){
 
+    if(req.body.task === 'add_fake_content'){
+        const users = await prisma.user.findMany()
+        const getRandomUser = () => {
+            const randomIndex = Math.floor(Math.random() * users.length)
+            return users[randomIndex]
+        }
+      const createPostFromRandomUser = async (subreddit) =>{
+          const user = getRandomUser()
+          return await prisma.post.create({
+            data: {
+                title:
+                faker.hacker.adjective() + ' ' +
+                faker.hacker.verb() + ' ' +
+                faker.hacker.noun(),
+                content: faker.hacker.phrase(),
+                subreddit: {
+                    connect: {
+                        name: subreddit.name,
+                    },
+                },
+                author: {
+                    connect: {id: user.id},
+                },
+            },
+          })
+      }
+
+      const createCommentsToPost = async (post) => {
+        let count = 0
+        const commentsNumber = Math.floor(Math.random() * 5)
+    
+        while (count < commentsNumber) {
+          const user = getRandomUser()
+    
+          await prisma.comment.create({
+            data: {
+              content: faker.hacker.phrase(),
+              post: {
+                connect: { id: post.id },
+              },
+              author: {
+                connect: { id: user.id },
+              },
+            },
+          })
+          count++
+        }
+      }
+
+      const subreddits = await prisma.subreddit.findMany()
+      
+      subreddits.forEach(async(subreddit) => {
+        const post = await createPostFromRandomUser(subreddit)
+        await createCommentsToPost(post)
+      })
     }
+
     if(req.body.task === 'clean_database'){
         await prisma.comment.deleteMany({})
         await prisma.post.deleteMany({})
