1bfb6510fddba9297f412bd9e5e402b558be2dbb
sharanjeetkaur <sharanjeet.kaur@lt.hs-fulda.de>
2023-04-28 13:19:44 +0200

    comment saved in database

---
+++ pages/api/comment.js
@@ -0,0 +1,34 @@
+import { getServerSession } from "next-auth"
+import { authOptions } from "./auth/[...nextauth]"
+
+export default async function handler(req, res) {
+    if (req.method !== 'POST') {
+        return res.status(501).end()
+    }
+
+    const session = await getServerSession(req, res, authOptions)
+    if(!session) return res.status(401).json({message: 'Not logged in'})
+    const user = await prisma.user.findUnique({
+        where: {
+            id: session.user.id
+        },
+    })
+    if(!user) return res.status(401).json({message: 'User not found'})
+    if(req.method === 'POST') {
+        const comment = await prisma.comment.create({
+            data: {
+               content: req.body.content,
+               post: {
+                connect: {
+                    id: req.body.post,
+                },
+               },
+               author: {
+                connect: {id: user.id},
+               },
+            },
+        })
+        res.json(comment)
+        return
+    }
+}
